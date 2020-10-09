56d45ea46e8530ad674e58a7d2cd5d8200d86e44
unknown <61050843@kmitl.ac.th>
2020-10-10 00:23:56 +0700

    atm-spring-code-config

---
+++ src/main/java/th/ac/kmitl/atm/Main.java
@@ -0,0 +1,18 @@
+package th.ac.kmitl.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+        ATM atm = context.getBean(ATM.class);
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
+        String name = atm.validateCustomer(2,2345);
+        System.out.println(name + " has " + atm.getBalance());
+        atm.withdraw(200);
+        System.out.println(name + " has " + atm.getBalance());
+    }
+}
